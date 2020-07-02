aeb8a2ebd26659790a4dd74bf7b312c2c0b79666
mbrandler <marya.brandler@yandex.ru>
2020-07-02 17:29:42 +0300

    Updates

---
+++ precache-manifest.34b1582428fdf6033c4600098b1540fa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54009e42c69e8d886a27628af65e6872",
+    "url": "/pharma-test/index.html"
+  },
+  {
+    "revision": "93f90e1f3bfd6edae854",
+    "url": "/pharma-test/static/css/main.9e78141c.chunk.css"
+  },
+  {
+    "revision": "3147333b240806ac6e76",
+    "url": "/pharma-test/static/js/2.2de8d1ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pharma-test/static/js/2.2de8d1ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93f90e1f3bfd6edae854",
+    "url": "/pharma-test/static/js/main.2dddec20.chunk.js"
+  },
+  {
+    "revision": "01e146a55cdf78b8ed95",
+    "url": "/pharma-test/static/js/runtime-main.229293c9.js"
+  },
+  {
+    "revision": "1e6ae1eead98df0dadce3c7f6b10aefc",
+    "url": "/pharma-test/static/media/Geometria-Bold.1e6ae1ee.ttf"
+  },
+  {
+    "revision": "7ed1493ebb9b3f936f025ce4285544a5",
+    "url": "/pharma-test/static/media/Geometria-Heavy.7ed1493e.ttf"
+  },
+  {
+    "revision": "86b1b85bdce1520f605f5ebe8ba1000c",
+    "url": "/pharma-test/static/media/Geometria-Medium.86b1b85b.ttf"
+  },
+  {
+    "revision": "4307d7bad82729169e304b76363c6df6",
+    "url": "/pharma-test/static/media/Geometria.4307d7ba.ttf"
+  },
+  {
+    "revision": "7d5edf12d5036dee3a1073152eeb2a04",
+    "url": "/pharma-test/static/media/arrow.7d5edf12.svg"
+  },
+  {
+    "revision": "c97c6b5651c719e05245c43c343d8f58",
+    "url": "/pharma-test/static/media/book.c97c6b56.svg"
+  },
+  {
+    "revision": "6156a4cc79073ec31ccf2c83b59cb305",
+    "url": "/pharma-test/static/media/check.6156a4cc.svg"
+  },
+  {
+    "revision": "c5fa7f2e51d744092ccf8ae34478f4c8",
+    "url": "/pharma-test/static/media/image.c5fa7f2e.png"
+  }
+]);
